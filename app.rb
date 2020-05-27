ac29c30f3bd34887117e2604b7efc18ebb5ec579
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-27 16:11:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions unless test?
+    set :session_secret, "item"
+  end
+  
+  get '/' do
+  erb :index
+  end
+  
+  post '/checkout' do
+    @sessions = session
+    item = params["item"]
+    @sessions[:item] = item
+    
+  end
 end
