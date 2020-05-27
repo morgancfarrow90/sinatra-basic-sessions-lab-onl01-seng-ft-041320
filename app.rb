d988624ca3c1ec90fce28c7a98f9386caaadfc9d
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-27 15:38:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  configure do
+    enable :sessions unless test?
+    set :session_secret, "secret"
+  end
+  
   get '/' do
   erb :index
   end
